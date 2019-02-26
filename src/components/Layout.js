9ef0175ddf8242e09070c00096f7022a9c3e9b0c
Alex Gustafson <alexjgustafson@gmail.com>
2019-02-25 22:41:17 -0500

    Remove starter content from navbar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + WordPress" />
+    <Helmet title="Good Dog Widget" />
     <Navbar />
     <div>{children}</div>
   </div>
